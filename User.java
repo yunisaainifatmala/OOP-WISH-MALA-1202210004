d15c2cf09da0939113d8fa35f6d30aabd7e4aee4
yunisaainifatmala <115875464+yunisaainifatmala@users.noreply.github.com>
2022-11-04 07:57:01 +0700

    Add files via upload

---
+++ User.java
@@ -0,0 +1,19 @@
+public class User {
+    String Name;
+    String PhoneNumber;
+
+    public String getName(){
+        this.Name = Name;
+    }
+
+    public String getPhoneNumber(){
+        this.PhoneNumber = PhoneNumber;
+    }
+
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+
+
+
+}
