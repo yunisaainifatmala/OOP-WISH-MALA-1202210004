59e8ca5f2b1302ec88c22e8dd4d646aa5fbd6687
yunisaainifatmala <115875464+yunisaainifatmala@users.noreply.github.com>
2022-12-10 22:08:20 +0700

    Add files via upload

---
+++ MODUL1_MALA/src-20221103T234608Z-001/src/Menu.java
@@ -0,0 +1,23 @@
+public class Menu {
+    public class barang {
+    }
+    String Name;
+    String Category;
+    String Price;
+
+    public void setName(String Name){
+        this.Name = Name;
+    }
+
+    public void setCategory(String Category){
+        this.Category = Category;
+    }
+
+    public void setPrice(String Price){
+        this.Price = Price;
+    }
+
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+
+
+}
